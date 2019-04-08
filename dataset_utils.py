77c8aa623b29d763927d8efbe007442a43cf44ed
Hélio <helio@omnidigitalis.com.br>
2019-04-08 01:45:46 -0400

    dataframes from GroupKFold indexes

--- dataset_utils.py
+++ dataset_utils.py
@@ -2,3 +2,9 @@ from pandas.io.json import json_normalize
 
 def flatten_json(dict):
 	return json_normalize(dict["data"], ["paragraphs", 'qas', "answers"], ["title", ["paragraphs", "context"], ["paragraphs", "qas", "question"]]).rename(columns={"text": "answer", "paragraphs.context": "context", "paragraphs.qas.question": "question"})
+
+def get_dataframe_folds(dataframe, indexes):
+	return None
+	
+def transform_dataframe_folds(dataframe, indexes):
+	return None
