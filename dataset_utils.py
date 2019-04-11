6626a0d2355d67d06ef755e92c32cb3a18f0b73d
Hélio <helio@omnidigitalis.com.br>
2019-04-10 23:16:30 -0400

    Melt dataset

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,10 +1,47 @@
 from pandas.io.json import json_normalize
 
-def flatten_json(dict):
-	return json_normalize(dict["data"], ["paragraphs", 'qas', "answers"], ["title", ["paragraphs", "context"], ["paragraphs", "qas", "question"]]).rename(columns={"text": "answer", "paragraphs.context": "context", "paragraphs.qas.question": "question"})
 
-def get_dataframe_folds(dataframe, indexes):
-	return None
-	
-def transform_dataframe_folds(dataframe, indexes):
-	return None
+def flatten_json(dict):
+    dataframe = json_normalize(dict["data"], ["paragraphs", 'qas', "answers"],
+                               ["title", ["paragraphs", "context"], ["paragraphs", "qas", "question"],
+                                ["paragraphs", "qas", "id"]]).rename(
+        columns={"text": "answer", "paragraphs.context": "context", "paragraphs.qas.question": "question",
+                 "paragraphs.qas.id": "id"})
+    dataframe["answer_start"] = dataframe["answer_start"].astype('str')
+    return dataframe
+
+
+def melt_dataframe(dataframe):
+    dataset = {"data": melt(dataframe, "title", "title", "paragraphs"), "version": 1.0}
+
+    for title in dataset["data"]:
+        title["paragraphs"] = melt(title["paragraphs"], "context", "context", "qas")
+        for paragraph in title["paragraphs"]:
+            paragraph["qas"] = melt(paragraph["qas"], ["question", "id"], ["question", "id"], "answers")
+            for question in paragraph["qas"]:
+                question["answers"] = melt(question["answers"], ["answer", "answer_start"], ["text", "answer_start"],
+                                           None)
+
+    return dataset
+
+
+def melt(dataframe, key, key_alias, payload_key):
+    result = []
+    groups = dataframe.groupby(key)
+    for name, group in groups:
+        obj = {}
+
+        if isinstance(key, str):
+            group.pop(key)
+            obj[key_alias] = name
+            if payload_key is not None:
+                obj[payload_key] = group
+        elif isinstance(key, list):
+            for index, k in enumerate(key):
+                group.pop(k)
+                obj[key_alias[index]] = name[index]
+            if payload_key is not None:
+                obj[payload_key] = group
+
+        result.append(obj)
+    return result
