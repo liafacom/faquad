5361fb4e64118871c234e562a6baf6143f09036f
Hélio <helio@omnidigitalis.com.br>
2019-04-14 14:39:43 -0400

    remove portions

--- aurelio/__main__.py
+++ aurelio/__main__.py
@@ -3,11 +3,12 @@ import os
 
 
 def main():
-    portions = ["10", "25", "50", "75", "100"]
+    portions = ["25", "50", "75"]
 
     for portion in portions:
         run(os.path.realpath("data/train/qa_facom_dataset_{}.json".format(portion)),
             os.path.realpath("data/dev/qa_facom_dataset_{}.json".format(portion)),
             portion)
+
 
 main()
