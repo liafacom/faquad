107750d4af9737e07f0147679a860437f56bbb06
Hélio <helio@omnidigitalis.com.br>
2019-05-11 14:51:43 -0400

    .

--- aurelio/__main__.py
+++ aurelio/__main__.py
@@ -2,6 +2,7 @@ from experiment import run_kfold
 from os.path import realpath
 from sys import argv
 
+
 def main():
     # Portions relative to Group Shuffle Split TEST size
     portions = [0.0, 0.25, 0.5, 0.75, 0.9]
@@ -11,9 +12,9 @@ def main():
                   train_dataset_path=realpath("data/train.json"),
                   dev_dataset_path=realpath("data/dev.json"),
                   serialization_dir=realpath("models/{}".format(argv[1])),
-                  reduce_train_dataset=False,
+                  reduce_train_dataset=True,
                   reduce_dev_dataset=True,
-                  expand_train_qas=True,
+                  expand_train_qas=False,
                   elmo=True,
                   dev_dataset_portion=portion,
                   embedding_dim=100)
